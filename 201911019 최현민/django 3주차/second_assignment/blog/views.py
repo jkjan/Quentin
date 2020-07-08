26cc2790e47774040f9d08731dd6152047118519
hyunmin <choihm9903@likelion.org>
2020-07-08 17:18:44 +0900

    hyunmin assignment

--- 201911019 최현민/django 3주차/second_assignment/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Blog
-# Create your views here.
-
-
-def home(request):
-    blogs = Blog.objects
-    return render(request, 'home.html', {'blogs': blogs})
--- 201911019 최현민/django 6주차/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\choih\desktop\django 4주차\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
